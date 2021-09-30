401f0bfd2fbc2ca697d9fa1b8aaaf5622c4bd19e
Aphilome <1.katrin.1@mail.ru>
2021-09-30 19:33:54 +0300

    Add IsSimpleNumber
    Позже доделаю

---
+++ lesson-1/IsSimpleNumber/IsSimpleNumber/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace IsSimpleNumber
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.InputEncoding = Encoding.Unicode;
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+            {
+                Console.OutputEncoding = Encoding.UTF8;
+                Console.InputEncoding = Encoding.UTF8;
+            }
+
+            ulong n;
+            do
+            {
+                Console.WriteLine("Введите целое положительное число");
+            } while (!ulong.TryParse(Console.ReadLine(), out n));
+
+            // тут видимо забыла d создать?
+            ulong i = 0; // внимтельнее. Разве по алгоритму 0 она равна?
+            while (i < n)
+            {
+                if (n % i == 0)
+                {
+                    d++;
+                }
+                
+                i++;
+            }
+            
+            // посленяя проверка d
+            
+            
+            //Console.WriteLine("Excellent");    
+        }
+    }
+}
