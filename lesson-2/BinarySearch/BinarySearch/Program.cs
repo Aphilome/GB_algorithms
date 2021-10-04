655775410b7d20148810104deef98f951f53def4
Aphilome <1.katrin.1@mail.ru>
2021-10-04 21:55:09 +0300

    Add BinarySearch

---
+++ lesson-2/BinarySearch/BinarySearch/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace BinarySearch
+{
+    class Program
+    {
+        static void Main()
+        {
+            var mas = Enumerable.Range(1, 20).ToArray();
+            var s = 5;
+            Console.WriteLine($"Search {s} = {BinarySearch(mas, s)}");
+            s = 19;
+            Console.WriteLine($"Search {s} = {BinarySearch(mas, s)}");
+            s = 55;
+            Console.WriteLine($"Search {s} = {BinarySearch(mas, s)}");
+        }
+        
+        // O(log(n))
+        public static int BinarySearch(int[] inputArray, int searchValue)
+        {
+            var min = 0;
+            var max = inputArray.Length - 1;
+            while (min <= max)
+            {
+                var mid = (min + max) / 2;
+                if (searchValue == inputArray[mid])
+                    return mid;
+                if (searchValue < inputArray[mid])
+                    max = mid - 1;
+                else
+                    min = mid + 1;
+            }
+            return -1;
+        }
+    }
+}
