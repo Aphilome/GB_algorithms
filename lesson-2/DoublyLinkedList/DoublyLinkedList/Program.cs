95aad4aac426445ecc2029963089db0d2820c79c
Aphilome <1.katrin.1@mail.ru>
2021-10-04 21:33:03 +0300

    Add DoublyLinkedList

---
+++ lesson-2/DoublyLinkedList/DoublyLinkedList/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DoublyLinkedList
+{
+    class Program
+    {
+        static void Main()
+        {
+            ILinkedList list = new DoublyLinkedList();
+            Console.WriteLine($"count: {list.GetCount()}");
+            list.AddNode(1);
+            list.AddNode(2);
+            list.AddNode(3);
+            Console.WriteLine($"count: {list.GetCount()}");
+            list.RemoveNode(0);
+            list.RemoveNode(1);
+            list.RemoveNode(1);
+            Console.WriteLine($"count: {list.GetCount()}");
+        }
+    }
+}
