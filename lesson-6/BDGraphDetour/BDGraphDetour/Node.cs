05b9cd1a670a4b9b72330388d22332d8b3a2a844
Aphilome <1.katrin.1@mail.ru>
2021-10-16 15:03:25 +0300

    Add BDGraphDetour

---
+++ lesson-6/BDGraphDetour/BDGraphDetour/Node.cs
@@ -0,0 +1,11 @@
+﻿namespace BDGraphDetour
+{
+    public class Node<T>
+    {
+        public T Value { get; set; }
+        
+        public Node<T>[] Neighbours { get; set; }
+        
+        public bool IsVisited { get; set; }
+    }
+}
