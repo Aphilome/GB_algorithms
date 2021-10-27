aa09e88f40b70f9e451a5f7805fe36b3a0de3c70
Aphilome <1.katrin.1@mail.ru>
2021-10-28 01:48:06 +0300

    Add BucketSort and ExternalSort

---
+++ lesson-8/BucketSort/BucketSort/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace BucketSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bucketSorter = new BucketSorter();
+            var externalSorter = new ExternalSort();
+            
+            var count = 100;
+            var mas = new int[count];
+            var rnd = new Random();
+            for (int i = 0; i < count; i++)
+                mas[i] = rnd.Next(int.MinValue, int.MaxValue);
+            
+            var res1 = bucketSorter.Sort(mas);
+            var res2 = externalSorter.Sort(mas);
+        }
+    }
+}
